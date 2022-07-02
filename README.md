28f6a4b96a44f9544b98d073a7051b401c5be85b
siiblis <aashishkupradhan@gmail.com>
2022-07-02 23:13:22 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-4agfzw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-4agfzw)
---
+++ src/app/sidenav-drawer-overview-example.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+/** @title Basic drawer */
+@Component({
+  selector: 'sidenav-drawer-overview-example',
+  templateUrl: 'sidenav-drawer-overview-example.html',
+  styleUrls: ['sidenav-drawer-overview-example.css'],
+})
+export class SidenavDrawerOverviewExample {}
+
+
+/**  Copyright 2022 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
